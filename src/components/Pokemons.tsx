7b69eabc8cd53ada61a9339df9320e8fb46bc7d4
Carlos Álvarez Vereterra <carlos.vereterra@stemyenergy.com>
2023-11-20 09:31:24 +0100

    Minor corrections

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -1,39 +1,60 @@
-import { useGetPokemons } from "../hooks/useGetPokemons";
+import {
+  useGetPokemons,
+  useGetPokemonsIncorrectInfiteRenders,
+  useGetPokemonsUseCallback,
+} from "../hooks/useGetPokemons";
 import { PokemonList } from "./PokemonList";
 import { useState } from "react";
 export const Pokemons = () => {
-  const { pokemons, setPokemons } = useGetPokemons();
-  const [inputPokemon, setinputPokemon] = useState("");
-  const HandleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setinputPokemon(e.target.value);
+  const {
+    pokemons,
+    requestStatus: { isLoading, isError },
+  } = useGetPokemons();
+  const [inputPokemon, setInputPokemon] = useState("");
+  const [isListSorted, setIsListSorted] = useState(false);
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputPokemon(e.target.value);
   };
-  const SortPokemons = () => {
-    const sortedPokemons = [...pokemons].sort((a, b) => {
-      return a.name.localeCompare(b.name);
-    });
-    setPokemons(sortedPokemons);
+
+  const handleSortPokemons = () => {
+    setIsListSorted(!isListSorted);
   };
-  const filteredPokemons = pokemons.filter((p) => {
-    return p.name.toLowerCase().includes(inputPokemon.toLowerCase());
-  });
-  if (pokemons.length === 0) return <h1>Loading Pokemons...</h1>;
+
+  const filteredPokemons = pokemons.filter((p) =>
+    p.name.toLowerCase().includes(inputPokemon.toLowerCase())
+  );
+
+  if (isLoading) return <h1>Loading Pokemons...</h1>;
+  if (isError) return <h1>Something went wrong</h1>;
+
   return (
     <>
       <div className="p-2 m-5">
-        <input
-          onChange={(e) => HandleChangeInput(e)}
-          className="mb-2 border-2 border-blue-800 mr-2"
-          type="text"
-          placeholder="Search for a pokemon"
-        />
-        <button
-          onClick={SortPokemons}
-          className="bg-blue-800 rounded-lg px-2 py-1 text-white hover:bg-opacity-80"
-        >
-          Sort your pokemons
-        </button>
+        <div className="flex items-center">
+          <input
+            onChange={(e) => handleChangeInput(e)}
+            className="mb-2 border-2 border-blue-800 mr-2"
+            type="text"
+            placeholder="Search for a pokemon"
+          />
+          <label>Sort Pokemons</label>
+          <input
+            type="checkbox"
+            onClick={handleSortPokemons}
+            className="bg-blue-800 rounded-lg px-2 ml-2 py-1 text-white hover:bg-opacity-80"
+          />
+        </div>
 
-        <PokemonList pokemons={filteredPokemons} />
+        <PokemonList
+          pokemons={
+            isListSorted
+              ? [...filteredPokemons].sort((a, b) =>
+                  a.name.localeCompare(b.name)
+                )
+              : filteredPokemons
+          }
+        />
       </div>
     </>
   );
--- src/hooks/useGetPokemons.ts
+++ src/hooks/useGetPokemons.ts
@@ -1,14 +1,70 @@
-import { PokemonType } from "../models/models";
+import { PokemonType } from "../types/pokemon";
 import { http } from "../services/http";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+
 export const useGetPokemons = () => {
   const [pokemons, setPokemons] = useState<PokemonType[]>([]);
-  const GetAllThePokemons = async () => {
+  const [requestStatus, setRequestStatus] = useState({
+    isLoading: false,
+    isError: false,
+  });
+
+  useEffect(() => {
+    // It's more common to define getAllPokemon inside useEffect,
+    // otherwise it would be defined every time the component is
+    // rendered and the state is updated
+
+    // Methods usually are camelCase, not PascalCase (besides React components)
+    const getAllPokemons = async () => {
+      setRequestStatus({ isLoading: true, isError: false });
+      const data = await http
+        .getPokemons()
+        .then((data) => {
+          setRequestStatus({ isLoading: false, isError: false });
+          return data;
+        })
+        .catch(() => setRequestStatus({ isLoading: false, isError: true }));
+
+      setPokemons(data.results);
+    };
+    getAllPokemons();
+  }, []);
+  return { pokemons, requestStatus };
+};
+
+// Other solution: useCallback avoids recreating the function every time
+// the component is rendered and the state is updated
+export const useGetPokemonsUseCallback = () => {
+  const [pokemons, setPokemons] = useState<PokemonType[]>([]);
+
+  const getAllPokemons = useCallback(async () => {
+    const data = await http.getPokemons();
+    setPokemons(data.results);
+  }, []);
+
+  useEffect(() => {
+    getAllPokemons();
+  }, [getAllPokemons]);
+  return { pokemons };
+};
+
+// Incorrect:
+// If you forget to use useCallback, you will get infite renders
+// because the function is in the dependency array and every time
+// the component is rendered, the function is recreated in memory,
+// even if the function is the same
+export const useGetPokemonsIncorrectInfiteRenders = () => {
+  const [pokemons, setPokemons] = useState<PokemonType[]>([]);
+
+  const getAllPokemons = async () => {
     const data = await http.getPokemons();
     setPokemons(data.results);
   };
+
+  console.log("Rendering, check console, you should see a lot of of logs");
+
   useEffect(() => {
-    GetAllThePokemons();
-  }, []);
-  return { pokemons, setPokemons };
+    getAllPokemons();
+  }, [getAllPokemons]);
+  return { pokemons };
 };
